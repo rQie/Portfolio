48a7d3109f75e3d2b6e1003c6237a46e8fc07fc8
rQie <rifqieakma@gmail.com>
2020-08-21 17:46:35 +0800

    create 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,22 @@
 import React from "react"
+import Layout from "../components/Layout"
+import { Link } from "gatsby"
+// import SEO from "../components/SEO"
 
 const Error = () => {
-  return <h2>error page</h2>
+  return (
+    <Layout>
+      {/* <SEO title="Dead End" /> */}
+      <main className="error-page">
+        <div className="error-container">
+          <h1>oops it`s error 404</h1>
+          <Link to="/" className="btn">
+            back home
+          </Link>
+        </div>
+      </main>
+    </Layout>
+  )
 }
 
 export default Error
